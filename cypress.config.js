dc5b79f6198a4fc81adca9305cdf32dbee13cb07
mlssb <arantesmelissab@hotmail.com>
2024-08-22 16:19:51 -0300

    primeiro commit apenas para configuração de ambiente

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://www.saucedemo.com/v1/', // URL base para os testes
+    viewportWidth: 1280,              // Largura da viewport do navegador
+    viewportHeight: 720,              // Altura da viewport do navegador
+    retries: {
+      runMode: 2,                     // Quantidade de tentativas em caso de falha durante o run mode
+      openMode: 0,                    // Quantidade de tentativas em caso de falha durante o open mode
+    },
+       
+    // Configurações do repórter
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports', // Diretório onde os relatórios serão gerados
+      overwrite: false,
+      html: true,
+      json: true,
+      reportFilename: 'report', // Nome do arquivo de relatório
+    },
+  },
+});
