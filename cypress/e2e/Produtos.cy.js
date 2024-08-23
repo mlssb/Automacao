2fe463fbc2db2ad86fb4fec615418f40cf2edb20
mlssb <arantesmelissab@hotmail.com>
2024-08-23 17:25:57 -0300

    Adição de comentários de cenário de teste em BDD

--- cypress/e2e/Produtos.cy.js
+++ cypress/e2e/Produtos.cy.js
@@ -20,18 +20,27 @@ describe('Product Page Tests', () => {
     });
 
     it('Deve verificar o nome, preço e descrição do primeiro produto', () => {
+
+        //Dado que usuário esteja na pagina inicial de produtos, quando visualizar listgem, então será possível conferir nome do produto, valor e sua descrição
+
         productPage.verifyProductName(1, 'Sauce Labs Backpack');
         productPage.verifyProductPrice(1, '$29.99');
         productPage.verifyProductDescription(1, 'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.');
     });
 
     it('Deve adicionar o primeiro produto ao carrinho', () => {
+
+        //Dado que usuário clique em "add to cart", quando clicar no carrinho, então encontrará item adicionado
+
         productPage.addToCart(1);
         cy.get('.fa-layers-counter').click()
         cy.get('.cart_item').should('contain','Sauce Labs Backpack');
        
     });
     it('Deve adicionar todos os produtos ao carrinho', () => {
+
+        //Dado que usuário tenha adicionado um produto de cada, quando visualizar ícone do carrinho, então a contagem corresponderá a quantidade de itens adicionados
+
         for (let i = 1; i <= 6; i++) {
             productPage.addToCart(i);
            
@@ -40,6 +49,9 @@ describe('Product Page Tests', () => {
     });
     
     it.only('Deve remover um produto do carrinho', () => {
+
+        //Dado que usuário esteja adicionando produtos ao carrinho, quando acionar botão remover, então item será excluido do carrinho, tendo sua contagem atualizada
+
 
         //adiciona os itens no carrinho
         for (let i = 1; i <= 3; i++) {
@@ -55,6 +67,9 @@ describe('Product Page Tests', () => {
    
 
     it ('Deve verificar que todos os itens de inventário tem nomes visíveis', () => {
+
+        //Dado que usuário esteja visualizando pagina de produtos, quando verificar itens disponíveis, então todos produtos terão seu nome exibidos 
+
         // ($el) variável que armazena cada elemento da classe inventory_item_name durante a iteração, para poder confirmar que todos itens existentes tem seu nome visível
         
         cy.get('.inventory_item_name').each(($el) => {
